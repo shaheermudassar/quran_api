847de19e950666e66687d571024f96d427f89a1d
shaheermudassar <shaheer.mudassar13@gmail.com>
2024-03-09 19:21:05 +0500

    updated

--- audio_compare_api/settings.py
+++ audio_compare_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$a(8i7_k#9i-!tk&52!1tnp^i^#-5eh6^jt1oog+c9@#1gtj$4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quran-api-3bfp.onrender.com']
+ALLOWED_HOSTS = ['quran-api-3bfp.onrender.com', '127.0.0.1']
 
 
 # Application definition
